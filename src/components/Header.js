a9df0b17534bc6043af5114eb35ecf60699569b3
Gurlabe <gurlabe72@gmail.com>
2019-04-30 15:44:46 -0700

    added react router link to main header contact button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Header, Segment, Image, Button } from "semantic-ui-react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const MainHeader = () => (
   <Segment inverted>
-    <Image inverted src="https://i.imgur.com/35UAXn4.png" />
-    <Header
-      as="h2"
-      inverted
-      style={{
-        fontWeight: "normal",
-        marginBottom: 0
-      }}
-    />
-    <Button basic color="blue" fluid>
-      Contact Us
-    </Button>
+    <Router>
+      <Image inverted src="https://i.imgur.com/35UAXn4.png" />
+      <Header
+        as="h2"
+        inverted
+        style={{
+          fontWeight: "normal",
+          marginBottom: 0
+        }}
+      />
+      <Link as="a" to="/ContactPage">
+        <Button color="blue" fluid>
+          Contact Us
+        </Button>
+      </Link>
+    </Router>
   </Segment>
 );
 
