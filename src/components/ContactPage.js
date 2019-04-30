2d09a9577ecd9287e5e931b0ee6261cce41a1372
Gurlabe <gurlabe72@gmail.com>
2019-04-30 15:52:02 -0700

    fixing how i set up the component for the contac page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,38 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import MainHeader from "./Header";
 import Navigation from "./Navbar";
 import Footer from "./Footer";
 import { Segment, Container, Form, Button } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 
-function ContactPage() {
-  return (
-    <Container>
-      <Segment inverted>
-        <Navigation />
-        <MainHeader />
-      </Segment>
-      <Segment inverted>
-        <Form inverted>
-          <Form.Group widths="equal">
-            <Form.Input
-              fluid
-              label="Name"
-              placeholder="First Name, Last Name"
-            />
-            <Form.Input
-              fluid
-              label="Name of Business or Brand"
-              placeholder="Tell us the name of your business"
-            />
-            <Form.TextBox fluid label="Tell us a little about your business" />
-          </Form.Group>
-          <Form.Checkbox label="I agree to the Terms and Conditions" />
-          <Button type="submit">Submit</Button>
-        </Form>
-        <Footer />
-      </Segment>
-    </Container>
-  );
+class ContactPage extends Component {
+  render() {
+    return (
+      <Container>
+        <Segment inverted>
+          <Navigation />
+          <MainHeader />
+        </Segment>
+        <Segment inverted>
+          <Form inverted>
+            <Form.Group widths="equal">
+              <Form.Input
+                fluid
+                label="Name"
+                placeholder="First Name, Last Name"
+              />
+              <Form.Input
+                fluid
+                label="Name of Business or Brand"
+                placeholder="Tell us the name of your business"
+              />
+              <Form.TextBox
+                fluid
+                label="Tell us a little about your business"
+              />
+            </Form.Group>
+            <Form.Checkbox label="I agree to the Terms and Conditions" />
+            <Button type="submit">Submit</Button>
+          </Form>
+          <Footer />
+        </Segment>
+      </Container>
+    );
+  }
 }
+
 export default ContactPage;
